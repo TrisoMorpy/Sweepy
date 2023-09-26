20a59f8aeecffeb13bf73dc65dd35af8706b18e8
TrisoMorpy <146025091+TrisoMorpy@users.noreply.github.com>
2023-09-26 20:26:53 +0100

    Update assistant.bat

--- assistant.bat
+++ assistant.bat
@@ -7,12 +7,12 @@ cls
 title Please wait...
 echo Sweepy is creating necessary files (you can delete these at any time).
 echo.
-echo [TASK 1]: Create TrisoSoft folder in (C:\Users\Public\Public Desktop). This may require admin (it will require admin because we're making a folder in a protected place).
-cd C:\Users\Public\Public Desktop
+echo [TASK 1]: Create TrisoSoft folder on your C drive.
+cd C:\
 mkdir TrisoSoft
 echo [TASK 1]: Complete.
 echo [TASK 2]: Making Sweepy folder.
-cd C:\Users\Public\Public Desktop\TrisoSoft
+cd C:\TrisoSoft
 mkdir Sweepy
 echo [TASK 2]: Complete.
 echo.
@@ -32,44 +32,47 @@ if %command% == test.command goto command.trisosoft.ftu.tutorial.testcommand
 :command.trisosoft.ftu.tutorial.testcommand
 cls
 title Sweepy
-echo Well done! You have sucessfully ran the following command "%command%".
+echo Well done! You have completed the tutorial. If you require more assistance (or spot a bug, yuck) please report it on our issue tracker.
 pause
 goto command.trisosoft.home
 :command.trisosoft.home
 cls
-title HOME | %waiting%
-echo Current time (local to your Sweepy install): %time%.
-echo Current date (local to your Sweepy install): %date%.
-echo.
-echo You can run the following command to get help: "help" (the help command will tell you all of the commands you can run on Sweepy).
+title HOME - %waiting%
+echo If you run the 'help' command you will be able to find the list of commands (the help command will tell you all of the commands you can run on Sweepy).
 set /p command=
 if %command% == home goto command.trisosoft.home
 if %command% == launch.setup goto command.trisosoft.ftu
 if %command% == help goto command.trisosoft.help
 if %command% == update goto command.trisosoft.update
+if %command% == issue.tracker goto command.trisosoft.issues
+:command.trisosoft.issues
+cls
+title Issue Tracker - %pleasewait%.
+echo Launching the issue tracker. 
+start "" https://github.com/TrisoSoft/Sweepy/issues
+goto command.trisosoft.home
 :command.trisosoft.help
 cls
-title Help Topics | %waiting%
+title Help Topics - %waiting%
 echo "home" - returns to the HOME menu.
 echo "launch.setup" - launches the setup program.
 echo "help" - launches the help program (the program you are currently running).
 echo "update" - attempts to update the program.
+echo "issue.tracker" - launches the Sweepy issue tracker.
 pause
 goto command.trisosoft.home
 :command.trisosoft.update
 cls
-title Update | %pleasewait%.
+title Update - %pleasewait%.
 echo Please wait whilst we connect to the internet. Your default web browser will be opened.
 echo [TASK 1]: Checking your internet connection - this will connect to "google.com"
 ping google.com
 echo [TASK 1]: Completed. If this didn't work, please make sure you are connected to the internet.
 pause
-title Update | %waiting%
-echo You are currently running KB2.
+title Update - %waiting%
+echo You are currently running KB3.
 echo.
-echo You have the current updates/feature updates/security updates.
-echo textOS System - (this can not be removed)
-echo September 2023 - KB1 (this can not be removed)
-goto "" https://github.com/TrisoSoft/Sweepy
+echo The Sweepy Dev team would like to thank you for using Sweepy.
+start "" https://github.com/TrisoSoft/Sweepy
 pause
 goto command.trisosoft.home
